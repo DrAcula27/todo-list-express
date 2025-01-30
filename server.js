ae051f6d3c90d71f2bb397481f60ac8469d3bdd0
Danielle <danielle@kendallspace.net>
2025-01-29 16:07:39 -0800

    Prettier formatting, fix typos, comment each line/block to explain what it does

--- server.js
+++ server.js
@@ -1,93 +1,142 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
-
-
+// import express
+const express = require('express');
+// import mongodb
+const MongoClient = require('mongodb').MongoClient;
+// set up dotenv
+require('dotenv').config();
+// use express
+const app = express();
+// set default port
+const PORT = 2121;
+// initialize db variables
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+  dbConnectionStr = process.env.DB_STRING,
+  dbName = 'todo';
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// connect to MongoDB database
+MongoClient.connect(dbConnectionStr, {
+  useUnifiedTopology: true,
+}).then((client) => {
+  console.log(`Connected to ${dbName} Database`);
+  db = client.db(dbName);
+});
 
+// let express know to expect ejs as view engine
+app.set('view engine', 'ejs');
+// tell express to use static files inside the public folder
+app.use(express.static('public'));
+// replace bodyparser middleware, this is how express can parse JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
-})
+// set up route for home page -> GET/Read
+// is async because need to await req to mongodb for todo items
+app.get('/', async (request, response) => {
+  // search db for all todo items, put into an array
+  const todoItems = await db.collection('todos').find().toArray();
+  // count how many todos are not done
+  const itemsLeft = await db
+    .collection('todos')
+    .countDocuments({ completed: false });
+  // render the DOM using the index.ejs template, supplying the todoItems as items and itemsLeft as left
+  response.render('index.ejs', { items: todoItems, left: itemsLeft });
+});
 
+// route to add a new todo item -> POST/Create
 app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
+  // access the todos db collection
+  db.collection('todos')
+    // insert one item (as thing) using the form to set the body, hard coding whether it is completed as false
+    .insertOne({ thing: request.body.todoItem, completed: false })
+    // after the item is inserted (async)
+    .then((result) => {
+      // print to the console that the todo was added
+      console.log('Todo Added');
+      // tell the client to redirect back to the home page (essentially a refresh) to cause a new GET request
+      response.redirect('/');
     })
-    .catch(error => console.error(error))
-})
+    // if something goes wrong, print the error to the console
+    .catch((error) => console.error(error));
+});
 
+// route to mark a todo as complete -> PUT/Update
 app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+  // access the todos db collection
+  db.collection('todos')
+    // update the first todo item found based on the todo text grabbed from the DOM on the client-side
+    .updateOne(
+      { thing: request.body.itemFromJS },
+      {
+        // set its completed attribute to true
         $set: {
-            completed: true
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
+          completed: true,
+        },
+      },
+      {
+        // sort the items in descending order
+        sort: { _id: -1 },
+        // if no item found, do not add a new one
+        upsert: false,
+      }
+    )
+    .then((result) => {
+      // print to the console that the item is marked complete
+      console.log('Marked Complete');
+      // respond to client that item marked complete
+      response.json('Marked Complete');
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+    // if something goes wrong, print error to console
+    .catch((error) => console.error(error));
+});
 
-})
-
+// route to mark a todo as uncompleted -> PUT/Update
 app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+  // access the todos db collection
+  db.collection('todos')
+    // update the first todo item found based on the todo text grabbed from the DOM on the client-side
+    .updateOne(
+      { thing: request.body.itemFromJS },
+      {
+        // set its completed attribute to false
         $set: {
-            completed: false
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
+          completed: false,
+        },
+      },
+      {
+        // sort the items in descending order
+        sort: { _id: -1 },
+        // if no item found, do not add a new one
+        upsert: false,
+      }
+    )
+    .then((result) => {
+      // print to the console that the item is marked uncompleted
+      console.log('Marked Uncompleted');
+      // respond to client that item marked uncompleted
+      response.json('Marked Uncompleted');
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+    // if something goes wrong, print error to console
+    .catch((error) => console.error(error));
+});
 
-})
-
+// route to remove a todo from the list -> DELETE/Dee-ley-tey
 app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
+  // access the todos db collection
+  db.collection('todos')
+    // remove the first todo item found based on the todo text grabbed from the DOM on the client-side
+    .deleteOne({ thing: request.body.itemFromJS })
+    .then((result) => {
+      // print to the console that the item was deleted
+      console.log('Todo Deleted');
+      // respond to client that item was deleted
+      response.json('Todo Deleted');
     })
-    .catch(error => console.error(error))
+    // if something goes wrong, print error to console
+    .catch((error) => console.error(error));
+});
 
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+// tell the server where to listen
+app.listen(process.env.PORT || PORT, () => {
+  // print listening statement to console
+  console.log(`Server running on port ${PORT}`);
+});
